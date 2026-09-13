ec9b6408e49b202f1204bbfcb42581c71757fcf3
Davison Jaramillo Gonzalez <davisonjaramillo592@gmail.com>
2026-09-13 18:46:23 -0500

    feat: inicializar proyecto Spring Boot - PoliShop

---
+++ src/main/java/co/edu/pcjic/polishop/PolishopApplication.java
@@ -0,0 +1,13 @@
+package co.edu.pcjic.polishop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolishopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PolishopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/edu/pcjic/polishop/PolishopApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.pcjic.polishop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PolishopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
